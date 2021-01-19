7df591fdf0221a03996e4ba745c481c3e71b9bfe
PhatNguyen <phatnt.uit@gmail.com>
2021-01-19 16:47:47 +0700

    fix: fix error handler

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const cors = require("cors");
-const morgan = require("morgan");
+const cors = require('cors');
+const morgan = require('morgan');
 const fetch = require('node-fetch');
 
-
 const PORT = process.env.PORT || 4000;
-const API_SERVICE_URL = "https://www.metaweather.com/api";
-
+const API_SERVICE_URL = 'https://www.metaweather.com/api';
 
 const app = express();
 
 // CORS
-app.use(cors({
-  origin: '*'
-}));
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
 
 // Logging
 app.use(morgan('dev'));
@@ -25,28 +25,34 @@ app.use('', (req, res, next) => {
   } else {
     res.status(403);
     res.json({
-      message: 'Authorization is required.'
+      message: 'Authorization is required.',
     });
   }
 });
 
+app.get('/meta-weather/*', async (req, res, next) => {
+  try {
+    const query = new URLSearchParams(req.query);
+    const url = `${API_SERVICE_URL}${req.path.replace(
+      '/meta-weather',
+      '',
+    )}?${query}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
 app.use((err, req, res, next) => {
-  res.status(500);
-  res.json({
-    message: err.message || 'Oops! Unknown error'
+  console.log('error', err);
+  return res.status(500).json({
+    message: err.message || 'Oops! Unknown error',
   });
 });
-
-app.get('/meta-weather/*', async (req, res) => {
-  const query = new URLSearchParams(req.query);
-  const url = `${API_SERVICE_URL}${req.path.replace('/meta-weather', '')}?${query}`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  res.json(data);
-});
-
 
 app.listen(PORT, () => {
   console.log(`Starting proxy at ${PORT}`);
